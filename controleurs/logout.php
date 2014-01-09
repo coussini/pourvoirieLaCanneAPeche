3af32d765c50dddada24828916815a4a5f908909
Florakasyan <florakasyan@gmail.com>
2014-01-09 10:06:55 -0500

    fichier logout pour utilisateur efface

--- controleurs/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION = array();	// initialisation du tableau des variables de la session client
-	session_destroy();		// destruction de la session
-	header("Location: ../index.php");
-?>
