6d9cfd31fb42b2a1d9c318a3fbcb13b22d3bb681
mvanhoye <mvanhoye@videotron.ca>
2014-01-09 12:45:00 -0500

    modification du contrôleur produits

--- controleurs/produits.class.php
+++ controleurs/produits.class.php
@@ -6,68 +6,71 @@ class Controleur
     {
         switch ($_GET['requete']) 
         {
-            case 'req_extraireDesReservations':
-                self::req_extraireDesReservations();
+            case 'req_extraireDesProduits':
+                self::req_extraireDesProduits();
                 break;        
-            case 'req_extraireUneReservation':
-                self::req_extraireUneReservation();
+            case 'req_extraireUnProduit':
+                self::req_extraireUnProduit();
                 break;        
-            case 'req_creerUneReservation':
-                self::req_creerUneReservation();
+            case 'req_creerUnProduit':
+                self::req_creerUnProduit();
                 break;        
-            case 'req_modifierUneReservation':
-                //req_modifierUneReservation();
+            case 'req_modifierUnProduit':
+                //req_modifierUnProduit();
                 break;        
             default:
                 // erreur
                 break;
         }
     }
-    // traitement extraire des réservations
-    private static function req_extraireDesReservations()
+    // traitement extraire des produits
+    private static function req_extraireDesProduits()
     {
         try
         {
-            $oReservations = new Reservations();
-            $reservations = $oReservations->extraireDesReservations($_GET["id_reservation"]);
-            VueReservations::formulaire_extraireDesReservations($reservations);
+            $oProduits = new Produits();
+            $Produits = $oProduits->extraireDesProduits($_GET["id_produit"]);
+            VueProduits::formulaire_extraireDesProduits($Produits);
         }
         catch(Exception $e)
         {
             $_GET['erreur']  = $e->getMessage();
-            VueReservations::formulaire_extraireDesReservations("");
+            VueProduits::formulaire_extraireDesProduits("");
         }
     }
 
-    // traitement extraire une réservation
-    private static function req_extraireUneReservation()
+    // traitement extraire un produit
+    private static function req_extraireUnProduit()
     {
         try
         {
-            $oReservations = new Reservations();
-            $reservations = $oReservations->extraireUneReservation();
-            VueReservations::formulaire_extraireUneReservation($reservations);
+            $oProduits = new Produits();
+            $Produits = $oProduits->extraireUnProduit();
+            VueProduits::formulaire_extraireUnProduit($produits);
         }
         catch(Exception $e)
         {
             $_GET['erreur']  = $e->getMessage();
-            VueReservations::formulaire_extraireUneReservation("");
+            VueProduits::formulaire_extraireUnProduit("");
         }
     }
 
     // traitement de confirmation d'un courriel dans le but d'y extraire les commandes
-    private static function req_creerUneReservation()
+    private static function req_creerUnProduit()
     {
         try
         {
-            $oReservations = new Reservations();            
-            $resultat = $oReservations->creerUneReservation($_GET["id_utilisateur"],$_GET["id_produit"],$_GET["date_debut"],$_GET["date_fin"],$_GET["nombre_de_semaine"]);
-            VueReservations::formulaire_creerUneReservation($resultat);
+            $oProduits = new Produits();            
+            $resultat = $oProduits->creerUnProduit($_GET["id_utilisateur"],
+			$_GET["id_produit"],$_GET["statut"],$_GET["image"],$_GET["nom"],
+			$_GET["emplacement"],$_GET["description"],$_GET["nombre_de_chambre"],
+			$_GET["nombre_de_salle_de_bain"],$_GET["prix_par_jour"],$_GET["prix_par_semaine"]);
+            VueProduits::formulaire_creerUnProduit($resultat);
         }
         catch(Exception $e)
         {
             $_GET['erreur']  = $e->getMessage();
-            VueReservations::formulaire_creerUneReservation("");
+            VueProduits::formulaire_creerUnProduit("");
         }
     }    
 }
