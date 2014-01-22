ee5435e0cb655592215350173173943f269f88f1
Mathieu De Grandpré <mathieudegrandpre@gmail.com>
2014-01-22 12:06:33 -0500

    popover chalets

--- js/jquery.js
+++ js/jquery.js
@@ -3,8 +3,6 @@ $(document).ready(function()
     // Attache évènement au select dans la page admin - contenu statique
 	$( "#selectNomContenu" ).change(function() {this.form.submit();});
 
-	// Popover
-	$(".monPopover").popover();
 
 });
 
